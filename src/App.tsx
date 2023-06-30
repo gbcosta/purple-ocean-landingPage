f12fbf1f4cc7ac5a04381d46ad51fa8a382836ef
Gabriel <gbcostasant@gmail.com>
2023-06-30 00:39:47 -0300

    add Home Image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Header from "./components/header/header";
-
+import HomeImage from "./components/homeImage/homeImage";
 function App() {
   return (
     <div>
-      <Header />
+      <Header className="mb-32" />
+      <HomeImage />
     </div>
   );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
-const Header = (): JSX.Element => {
+const Header = (props: { className: string }): JSX.Element => {
   return (
-    <div className="flex flex-col text-center mt-4">
+    <div className={`${props.className} flex flex-col text-center mt-24`}>
       <h1 className="text-6xl text-yellow mb-4">Purple Ocean</h1>
       <p className="text-xl text-white">
         A purple ocean theme for Visual Studio Code
