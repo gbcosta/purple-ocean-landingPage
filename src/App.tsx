79fa62403cd2e5ecd7cd0950a209b06773fbfe25
Gabriel <gbcostasant@gmail.com>
2023-06-29 23:52:42 -0300

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/header/header";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,10 @@
+const Header = (): JSX.Element => {
+  return (
+    <div>
+      <h1 className="text-3xl fontbold underline">Purple Ocean</h1>
+      <p>A purple ocean theme for Visual Studio Code</p>
+    </div>
+  );
+};
+
+export default Header;
