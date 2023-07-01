28457260cc33857266ca9fcceb97df35fe91395b
Gabriel <gbcostasant@gmail.com>
2023-06-30 21:41:22 -0300

    update cardContainer

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -7,7 +7,7 @@ const onClick = (): void => {
 const Card = (props: { imageSrc: string }): JSX.Element => {
   return (
     <figure
-      className="bg-purple-600 w-full flex justify-center items-center rounded-md cursor-pointer justify-self-center flex-col lg:pt-12 lg:pb-12 sm:pt-8 sm-pb:8"
+      className="bg-purple-600 w-full flex justify-center items-center rounded-md cursor-pointer justify-self-center flex-col lg:py-12  sm:py-8  max-sm:py-4 "
       onClick={onClick}
     >
       <img src={props.imageSrc} className="w-20 h-20" />
