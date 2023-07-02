b3c4e3c813fa1b23573dbc6807edb0abefa3c793
Gabriel <gbcostasant@gmail.com>
2023-07-02 02:08:14 -0300

    update color palette

--- src/components/colorPalette/colorPalette.tsx
+++ src/components/colorPalette/colorPalette.tsx
@@ -1,28 +1,30 @@
 const ColorBall = (props: { color: string }): JSX.Element => {
-  return <div className={`${props.color} rounded-full w-8 h-8`}></div>;
+  return <div className={`${props.color} rounded-full w-10 h-10`}></div>;
 };
 
 const ColorPalette = (): JSX.Element => {
   return (
-    <div className="flex items-center flex-col gap-4 mt-32">
-      <h2 className="text-white text-4xl">Color Palette</h2>
-      <div className="flex gap-4 mt-16">
-        <ColorBall color="bg-yellow" />
-        <ColorBall color="bg-palePurple" />
-        <ColorBall color="bg-red" />
-        <ColorBall color="bg-green" />
-        <ColorBall color="bg-lightGreen" />
-        <ColorBall color="bg-lightBlue" />
-        <ColorBall color="bg-white" />
-      </div>
-      <div className="flex gap-4">
-        <ColorBall color="bg-primary" />
-        <ColorBall color="bg-darkPurple" />
-        <ColorBall color="bg-secondary" />
-        <ColorBall color="bg-terciary" />
-        <ColorBall color="bg-quaternary" />
-        <ColorBall color="bg-lightPurple" />
-        <ColorBall color="bg-pink" />
+    <div className="mt-32">
+      <h2 className="text-white text-4xl text-center">Color Palette</h2>
+      <div className="flex items-center justify-center flex-col max-sm:flex-row gap-6 mt-16">
+        <div className="flex gap-6 flex-row max-sm:flex-col">
+          <ColorBall color="bg-yellow" />
+          <ColorBall color="bg-palePurple" />
+          <ColorBall color="bg-red" />
+          <ColorBall color="bg-green" />
+          <ColorBall color="bg-lightGreen" />
+          <ColorBall color="bg-lightBlue" />
+          <ColorBall color="bg-white" />
+        </div>
+        <div className="flex gap-6 flex-row max-sm:flex-col">
+          <ColorBall color="bg-primary" />
+          <ColorBall color="bg-darkPurple" />
+          <ColorBall color="bg-secondary" />
+          <ColorBall color="bg-terciary" />
+          <ColorBall color="bg-quaternary" />
+          <ColorBall color="bg-lightPurple" />
+          <ColorBall color="bg-pink" />
+        </div>
       </div>
     </div>
   );
